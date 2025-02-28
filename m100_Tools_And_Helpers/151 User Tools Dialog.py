605321ec15477bc8b08320cf15599819769c5fc3
robscarisbrick <robscarisbrick@gmail.com>
2025-02-28 08:50:24 +0000

    Rename

---
+++ m100_Tools_And_Helpers/151 User Tools Dialog.py
@@ -0,0 +1,17 @@
+import User_Tools_Library as lib
+from tkinter import *
+import win32com.client as win32
+lusas = win32.gencache.EnsureDispatch("Lusas.Modeller.22.0")
+lib.initialise(lusas)
+
+
+root = Tk()
+root.title('User Tools')
+root.geometry("400x600")
+
+Button(root, text='Match Surface Assignments', command=lambda:lib.match_assignments_surface(copy_mesh=True)).pack(padx=20, pady=20)
+
+Button(root, text='Match Line Assignments', command=lambda:lib.match_assignments_line(copy_mesh=True)).pack(padx=20, pady=20)
+
+
+root.mainloop()
